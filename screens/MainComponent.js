3d301dbcd85d629f6a81dc3c0949e7bdb86bb566
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-06-17 03:46:28 -0700

    home page added

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,21 +1,36 @@
 import { Platform, View } from 'react-native';
 import Constants from 'expo-constants';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './HomeScreen';
 import SiteInfoScreen from './SiteInfoScreen';
 import TourScreen from './TourScreen';
 
+const Drawer = createDrawerNavigator();
+    const screenOptions = {
+        headerTintColor: '#fff',
+        headerStyle: { backgroundColor: '#5637DD' }
+    }
+
+const HomeNavigator = () => {
+    const Stack = createStackNavigator();
+    return(
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Home'
+                component={HomeScreen}
+                options={{ title: 'Home' }}
+            />
+        </Stack.Navigator>
+    )
+}
 
 const TourNavigator = () => {
     const Stack = createStackNavigator();
         return(
             <Stack.Navigator
                 initialRouteName='Tour'
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: '#5637DD'
-                    },
-                    headerTintColor: '#fff'
-                }}>
+                screenOptions={screenOptions}>
                 <Stack.Screen
                     name='Tour'
                     component={TourScreen}
@@ -36,7 +51,20 @@ const Main = () => {
 
     return (
         <View style={{ flex: 1, paddingTop:Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
-            <TourNavigator />
+            <Drawer.Navigator
+                initialRouteName='Home'
+                drawerStyle={{ backgroundColor: '#CEC8FF' }}>
+                <Drawer.Screen
+                    name='Home'
+                    component={HomeNavigator}
+                    options={{ title: 'Home' }}
+                />
+                <Drawer.Screen
+                    name='Tour'
+                    component={TourNavigator}
+                    options={{ title: 'Tour' }}
+                />
+            </Drawer.Navigator>
         </View>
     );
 };
