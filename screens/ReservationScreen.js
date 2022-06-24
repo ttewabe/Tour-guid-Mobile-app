2adc5d2f6eaf7ce80a6abf30c6ebb58fde910760
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-06-24 04:05:43 -0700

    Modal

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Text, View, ScrollView, StyleSheet, Switch, Button } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Switch, Button, Modal } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
@@ -8,6 +8,7 @@ function ReservationScreen(){
     const [tourIn, setTourIn] = useState(false);
     const [date, setDate] = useState(new Date());
     const [showCalendar, setShowCalendar] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
     const onDateChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
@@ -23,6 +24,15 @@ function ReservationScreen(){
         setTourIn(false);
         setDate(new Date());
         setShowCalendar(false);
+        setShowModal(!showModal);
+    }
+
+    const resetForm = () => {
+        setVisitors(1);
+        setTourIn(false);
+        setDate(new Date());
+        setShowCalendar(false);
+
     }
     return (
         <ScrollView>
@@ -76,6 +86,36 @@ function ReservationScreen(){
                     accessibilityLabel='Tap me to search for available sites to reserve'
                 />
                 </View>
+                <Modal
+                    animationType='slide'
+                    transparent={false}
+                    visible={showModal}
+                    onRequestClose={() => setShowModal(!showModal)}>
+                
+                <View style={styles.modal}>
+                    <Text style={styles.modalTitle}>
+                        Search Tour Site Reservations
+                    </Text>
+                    <Text style={styles.modalText}>
+                        Number of Visitors: {visitors}
+                    </Text>
+                    <Text style={styles.modalText}>
+                        Tour-In?: {tourIn ? 'Yes' : 'No'}
+                    </Text>
+                    <Text style={styles.modalText}>
+                        Date: {date.toLocaleDateString('en-US')}
+                    </Text>
+
+                    <Button
+                        onPress={() => {
+                            setShowModal(!showModal);
+                            resetForm();
+                        }}
+                        color='#5637DD'
+                        title='Close'
+                    />
+                </View>
+            </Modal>
         </ScrollView>
     )
 }
@@ -94,9 +134,24 @@ const styles = StyleSheet.create({
     },
     formItem: {
         flex: 1
+    },
+
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    },
+    modalTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        backgroundColor: '#5637DD',
+        textAlign: 'center',
+        color: '#fff',
+        marginBottom: 20
+    },
+    modalText: {
+        fontSize: 18,
+        margin: 10
     }
 });
-  
-
 
 export default ReservationScreen;
